31034b2827d7a3bedf9029e3c5a6ad589c965f37
rizespb <rizespb@yandex.ru>
2024-04-18 09:44:40 +0300

    Udgraded client deps

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,20 @@
-import { useEffect } from 'react'
-import './App.css'
+import { ReactElement, useEffect } from 'react';
+import './App.css';
 
-function App() {
+function App(): ReactElement {
   useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+    const fetchServerData = async (): Promise<void> => {
+      const url = `http://localhost:${__SERVER_PORT__}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    };
 
-    fetchServerData()
-  }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+    fetchServerData();
+  }, []);
+  return <div className="App">Вот тут будет жить ваше приложение :)</div>;
 }
 
-export default App
+export default App;
+
+// const a = () => {};
