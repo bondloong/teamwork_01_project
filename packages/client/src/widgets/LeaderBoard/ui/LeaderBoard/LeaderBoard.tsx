3546471d9f71db86d8560518275fe2b4e96e1cd4
rizespb <rizespb@yandex.ru>
2024-04-22 10:44:51 +0300

    Fixed types

--- packages/client/src/widgets/LeaderBoard/ui/LeaderBoard/LeaderBoard.tsx
+++ packages/client/src/widgets/LeaderBoard/ui/LeaderBoard/LeaderBoard.tsx
@@ -2,9 +2,10 @@ import { Table } from 'antd';
 import { ReactElement } from 'react';
 import { tableColumns } from './LeaderBoard.constants';
 import { dataSourceMock } from './mocks';
+import { TLeaderBoardItem } from './LeaderBoard.interfaces';
 
 export const LeaderBoard = (): ReactElement => {
-  const dataWithRanks = dataSourceMock.map((item, index) => ({
+  const dataWithRanks = dataSourceMock.map<TLeaderBoardItem>((item, index) => ({
     ...item,
     position: index + 1,
     key: index,
