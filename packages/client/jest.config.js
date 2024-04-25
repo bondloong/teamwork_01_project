f9e39cee2f479f324e79946822d61e07860a9131
rizespb <rizespb@yandex.ru>
2024-04-25 16:58:02 +0300

    Fixed Application tests

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -10,7 +10,7 @@ export default {
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|sass|scss|gif)$': '<rootDir>/src/shared/tests/stubs/styleMock.ts',
-    '\\.(png|webp|jgp|jpeg|gif)$': '<rootDir>/src/shared/tests/stubs/imageStub.ts',
+    '\\.(css|less|sass|scss)$': '<rootDir>/src/shared/tests/stubs/styleMock.ts',
+    '\\.(png|webp|jpg|jpeg|gif)$': '<rootDir>/src/shared/tests/stubs/imageStub.ts',
   },
 };
