79e6f29fed088f73ad757a03966a0d5caa51893f
Aimen <mizou9999@yandex.ru>
2024-05-19 14:31:55 +0300

    add error page

--- packages/client/src/pages/NotFoundPage/ui/NotFoundPage/NotFoundPage.tsx
+++ packages/client/src/pages/NotFoundPage/ui/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,20 @@
 import { ReactElement } from 'react';
+import { Result, Button } from 'antd';
+import { Link } from 'react-router-dom';
+import { TEXTS } from './NotFoundPage.constants';
 
 export const NotFoundPage = (): ReactElement => {
-  return <h1>NotFoundPage</h1>;
+  const { title, button } = TEXTS;
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle={title}
+      extra={
+        <Link to="/">
+          <Button type="primary">{button}</Button>
+        </Link>
+      }
+    />
+  );
 };
