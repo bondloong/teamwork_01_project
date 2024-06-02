61b1ebba0792c6da0ca033fca67d2fdfcab7cc06
kheda <kheda.amirova@gmail.com>
2024-06-02 20:28:42 +0300

    Jest config add mp3

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -11,6 +11,6 @@ export default {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|sass|scss)$': '<rootDir>/src/shared/tests/stubs/styleMock.ts',
-    '\\.(png|webp|jpg|jpeg|gif)$': '<rootDir>/src/shared/tests/stubs/imageStub.ts',
+    '\\.(png|webp|jpg|jpeg|gif|mp3)$': '<rootDir>/src/shared/tests/stubs/imageStub.ts',
   },
 };
