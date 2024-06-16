cf32ba0e43ee434ac266cc3715760c8033788c21
rizespb <rizespb@yandex.ru>
2024-06-16 15:44:36 +0300

    docker-compose for client

--- packages/client/src/app/ui/Application/Application.tsx
+++ packages/client/src/app/ui/Application/Application.tsx
@@ -6,7 +6,7 @@ import { LeaderboardPage } from '@/pages/LeaderboardPage';
 import { NotFoundPage } from '@/pages/NotFoundPage';
 import { TopicPage } from '@/pages/TopicPage';
 import { MainPage } from '@/pages/MainPage';
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import '../../styles/_app.scss';
 import { ProtectedRoute } from '@/widgets/ProtectedRoute';
@@ -17,6 +17,18 @@ import { Auth } from '../Auth';
 
 export const Application = (): ReactElement => {
   useServiceWorker();
+
+  // @TODO Это пример работы с env-переменными для общения с сервером
+  // Удалить после того, как будет реализовано апи форума и темизации
+  // docker-compose установит связь между контейнерами. Контейнеры смогут общаться между собой по имени контейнера. Поэтому используем SERVER_HOST, который также является именем контейнера для контейнера с апи-сервером
+  useEffect(() => {
+    const host = import.meta.env.SERVER_HOST || 'localhost';
+    const port = import.meta.env.SERVER_PORT || '3001';
+
+    fetch(`http://${host}:${port}/api`)
+      .then((res) => res.json())
+      .then(console.log);
+  }, []);
 
   return (
     <StoreProvider>
