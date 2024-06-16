048a5520a163bc90ed4b358c1a3808e8d787019c
rizespb <rizespb@yandex.ru>
2024-06-16 14:47:18 +0300

    docker-compose for mongo and server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,31 +1,45 @@
 import dotenv from 'dotenv';
 import cors from 'cors';
-import path from 'path';
 dotenv.config();
 
 import express from 'express';
-import { createClientAndConnect } from './db';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(cors());
 const port = Number(process.env.SERVER_PORT) || 3001;
 
-createClientAndConnect();
-
-app.use(
-  express.static(path.join(__dirname, '..', 'client', 'dist'), {
-    extensions: ['html'],
-  })
-);
-
-app.get('/api', (_, res) => {
-  res.json('👋 Howdy from the server :)');
+app.use('/api', (_, res) => {
+  res.json('👋 Howdy from the server API:)');
 });
 
 app.get('*', (_, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'));
+  res.json('You are awesome)');
 });
 
-app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
-});
+const dbUrl = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
+
+const dbName = process.env.MONGO_INITDB_DATABASE;
+const client = new MongoClient(dbUrl);
+
+const connectDb = async (onSuccess: () => void): Promise<void> => {
+  try {
+    await client.connect();
+    console.log('Connected successfully to datebase');
+
+    onSuccess();
+
+    const db = client.db(dbName);
+    console.log('Database: ', db);
+  } catch (error) {
+    console.log('Could not connect to datebase', error);
+  }
+};
+
+const startApp = (): void => {
+  app.listen(port, () => {
+    console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
+  });
+};
+
+connectDb(startApp);
