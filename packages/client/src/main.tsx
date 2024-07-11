2e8b78c929f4d9ec21419614a0e6145c79b98953
Aimen <mizou9999@yandex.ru>
2024-07-11 18:34:05 +0300

    ok

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/styles/_reset.scss';
+import './app/styles/_theme.scss';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createReduxStore } from './app/model';
 import { Provider } from 'react-redux';
 import { routes } from './app/model/constants/routes';
 import { Application } from './app';
+import { ThemeProvider } from '@/shared/hooks/theme';
 
 const router = createBrowserRouter(routes);
 
@@ -16,9 +18,11 @@ ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <React.StrictMode>
     <Provider store={store}>
-      <Application>
-        <RouterProvider router={router} />
-      </Application>
+      <ThemeProvider>
+        <Application>
+          <RouterProvider router={router} />
+        </Application>
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
